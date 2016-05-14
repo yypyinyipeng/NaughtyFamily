2a42fea400de63ee6b8db6846c1c88c5720ff6db
YinYiPeng <18845296347@163.com>
2016-05-14 23:11:33 +0800

    updateAreas

---
+++ Areas/Area/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NaughtyFamily.Areas.Area.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.currentTime = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult insert()
+        {
+            return View();
+        }
+
+        public ActionResult design()
+        {
+            return View();
+        }
+
+        public ActionResult system()
+        {
+            return View();
+        }
+    }
+}
